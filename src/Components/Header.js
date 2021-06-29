47c89ac5fa846a2fb77be1a0b51eed0b326af6b9
dnggngn825 <53506074+dnggngn825@users.noreply.github.com>
2021-06-29 23:32:31 +1000

    .

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Header() {
+    return (
+        <nav>
+            <h1>Hello World</h1>
+            <ul>
+                <li>thing 1</li>
+                <li>thing 2</li>
+                <li>thing 3</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+import App from "./Components/App"
+// import MyInfo from "./Components/MyInfo"
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById("root"))
