df632fbd2ef6fb6cf0179ae60cef46e5c30f8d31
dnggngn825 <53506074+dnggngn825@users.noreply.github.com>
2021-07-04 13:26:10 +1000

    commit from bash

--- src/Day 12 todo phase 56/Components/toDoItem.js
+++ src/Day 12 todo phase 56/Components/toDoItem.js
@@ -3,6 +3,12 @@ import style from "../index.css";
 
 
 function toDoItem(props) {
+    const CompleteStyle = props.info.completed? 
+                {textDecorationLine: 'line-through', 
+                textDecorationStyle: 'solid',
+                fontStyle: 'italic',
+                fontWeight: '200',
+                color: 'grey'} : {}
     return (
         <div className="todo-item">
             <input 
@@ -10,7 +16,7 @@ function toDoItem(props) {
                 checked={props.info.completed} 
                 onChange={() => props.passFunction(props.info.id)}  // it will fire an event, so props.passFunction is not good enough
             />
-            <p>{props.info.text}</p>
+            <p style={CompleteStyle}>{props.info.text}</p>
         </div>
     )
 }
